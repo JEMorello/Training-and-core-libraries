e73951f6f977d97074189b29cc6f25b54d406a27
Jason Morello <jason.brenton1@gmail.com>
2017-03-21 16:13:21 -0400

    Initial Template from SharpDevelop.

---
+++ GenericWebShop/GenericWebShop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JMorello
+ * Date: 3/21/2017
+ * Time: 4:06 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Web.Mvc;
+
+namespace GenericWebShop.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		
+		public ActionResult Contact()
+		{
+			return View();
+		}
+	}
+}
---
+++ GenericWebShop/GenericWebShop/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JMorello
+ * Date: 3/21/2017
+ * Time: 4:06 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GenericWebShop
+{
+	public class MvcApplication : HttpApplication
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.Ignore("{resource}.axd/{*pathInfo}");
+			
+			routes.MapRoute(
+				"Default",
+				"{controller}/{action}/{id}",
+				new {
+					controller = "Home",
+					action = "Index",
+					id = UrlParameter.Optional
+				});
+		}
+		
+		protected void Application_Start()
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}
